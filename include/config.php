ea09178070ddd33d4bbf11c97e87a63376b6f4c0
ignatius93 <ignatiusroni12@gmail.com>
2025-05-12 22:36:35 +0700

    Add files via upload

---
+++ include/config.php
@@ -0,0 +1,17 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<Admin','mikhmon>|>eZWfoZ9jamQ=');
+
+$data['Selayu'] = array ('1'=>'Selayu!remote7.vpnmurahjogja.my.id:32241','Selayu@|@admin','Selayu#|#amRjaGNk','Selayu%Selayu','Selayu^hotspot.com','Selayu&Rp','Selayu*10','Selayu(1','Selayu)','Selayu=disable','Selayu@!@enable');
+$data['Temiasio'] = array ('1'=>'Temiasio!remote7.vpnmurahjogja.my.id:38170','Temiasio@|@admin','Temiasio#|#aWNiaWpl','Temiasio%Temiasio','Temiasio^hotspot.com','Temiasio&Rp','Temiasio*10','Temiasio(1','Temiasio)','Temiasio=disable','Temiasio@!@enable');
+$data['Sepang'] = array ('1'=>'Sepang!remote8.vpnmurahjogja.my.id:34161','Sepang@|@admin','Sepang#|#aWNiaWpl','Sepang%Sepang','Sepang^hotspot.com','Sepang&Rp','Sepang*10','Sepang(1','Sepang)','Sepang=disable','Sepang@!@enable');
+$data['Molo'] = array ('1'=>'Molo!remote8.vpnmurahjogja.my.id:3051','Molo@|@admin','Molo#|#aWNiaWpl','Molo%Molo','Molo^hotspot.com','Molo&Rp','Molo*10','Molo(1','Molo)','Molo=disable','Molo@!@enable');
+$data['Bengkawe'] = array ('1'=>'Bengkawe!remote7.vpnmurahjogja.my.id:3863','Bengkawe@|@admin','Bengkawe#|#jJqfp5WTm5mbaWY=','Bengkawe%Bengkawe','Bengkawe^hotspot.com','Bengkawe&Rp','Bengkawe*10','Bengkawe(1','Bengkawe)','Bengkawe=disable','Bengkawe@!@enable');
+$data['Jelatok'] = array ('1'=>'Jelatok!remote8.vpnmurahjogja.my.id:3635','Jelatok@|@admin','Jelatok#|#aWNiaWpl','Jelatok%Jelatok','Jelatok^hotspot.com','Jelatok&Rp','Jelatok*10','Jelatok(1','Jelatok)','Jelatok=disable','Jelatok@!@enable');
+$data['Bajirak'] = array ('1'=>'Bajirak!remote6.vpnmurahjogja.my.id:36109','Bajirak@|@admin','Bajirak#|#aWNiaWpl','Bajirak%Bajirak','Bajirak^hotspot.com','Bajirak&Rp','Bajirak*10','Bajirak(1','Bajirak)','Bajirak=disable','Bajirak@!@enable');
+$data['Samatakin'] = array ('1'=>'Samatakin!remote8.vpnmurahjogja.my.id:36157','Samatakin@|@admin','Samatakin#|#aWNiaWpl','Samatakin%Samatakin','Samatakin^hotspot.com','Samatakin&Rp','Samatakin*10','Samatakin(1','Samatakin)','Samatakin=disable','Samatakin@!@enable');
+$data['Menjalin'] = array ('1'=>'Menjalin!remote8.vpnmurahjogja.my.id:3358','Menjalin@|@admin','Menjalin#|#jJqfp5WTm5mbaWY=','Menjalin%Menjalin','Menjalin^hotspot.com','Menjalin&Rp','Menjalin*10','Menjalin(1','Menjalin)','Menjalin=disable','Menjalin@!@enable');
+$data['Bemuran'] = array ('1'=>'Bemuran!remote8.vpnmurahjogja.my.id:3320','Bemuran@|@admin','Bemuran#|#aWNiaWpl','Bemuran%Bemuran','Bemuran^hotspot.com','Bemuran&Rp','Bemuran*10','Bemuran(1','Bemuran)','Bemuran=disable','Bemuran@!@enable');
+
+$data['Mereseng'] = array ('1'=>'Mereseng!remote8.vpnmurahjogja.my.id:31155','Mereseng@|@admin','Mereseng#|#aWNiaWpl','Mereseng%Mereseng','Mereseng^hotspot.com','Mereseng&Rp','Mereseng*10','Mereseng(1','Mereseng)','Mereseng=disable','Mereseng@!@enable');
+$data['karangan'] = array ('1'=>'karangan!remote9.vpnmurahjogja.my.id:3799','karangan@|@admin','karangan#|#aWNiaWpl','karangan%karangan','karangan^hotspot.com','karangan&Rp','karangan*10','karangan(1','karangan)','karangan=10','karangan@!@disable');
